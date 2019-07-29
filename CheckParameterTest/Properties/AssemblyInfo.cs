a468161d32658e553fb52aba8952827432ef9849
Sreelal.ES <sreethered@gmail.com>
2019-07-29 14:14:21 +0530

    catching up

---
+++ CheckParameterTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CheckParameterTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Philips Healthcare")]
+[assembly: AssemblyProduct("CheckParameterTest")]
+[assembly: AssemblyCopyright("Copyright © Philips Healthcare 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5e14711-d669-4939-b4e1-4c01e0a40c24")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
