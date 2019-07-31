5a1e71f34ebc2b9a8a38df9f59e0dd2f703797b8
Sreelal.ES <sreethered@gmail.com>
2019-07-31 14:39:20 +0530

    Batch file updated with action to run tests

--- PatientMonitor-master/AutoBuildTest.bat
+++ PatientMonitor-master/AutoBuildTest.bat
@@ -6,7 +6,7 @@ set "BAT_PATH=%~dp0"
 @echo off
 
 
-call "<C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat>"
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 
 echo %BAT_PATH%
 
@@ -17,7 +17,7 @@ echo Executing batch file
 :: param2 =     Build Type: default Debug, Range: Debug or Release
 ::Win32
 ::              
-To Make it to work open BranchConfiguration.json then set the value of "name": "BranchProfile" 
+:: To Make it to work open BranchConfiguration.json then set the value of "name": "BranchProfile" 
 
 ::              to as per Build Type like, "value": "Release" or "value": "Debug", By default the vlaue is debug
 ::X64
@@ -30,6 +30,8 @@ To Make it to work open BranchConfiguration.json then set the value of "name": "
 :: param4 =     Platform: default: x64 or Win32, Range: Win32 OR x64
 
 devenv "%BAT_PATH%\Alerting.sln" /build Debug 
+mstest /TestContainer:"%BAT_PATH%PatientMoniterTest\bin\Debug\PatientMoniterTest.dll" /Test:SPO2ValidTC /Test:SPO2InvalidTC /Test:SPO2BoundaryTCMinus1 /Test:SPO2BoundaryTCZero /Test:SPO2BoundaryTCPlusOne /Test:TempValidTC /Test:TempInvalidTC /Test:TempBoundaryTCMinus1 /Test:TempBoundaryTCZero /Test:TempBoundaryTCPlusOne /Test:PulseValidTC /Test:PulseInvalidTC /Test:PulseBoundaryTCMinus1 /Test:PulseBoundaryTCZero /Test:PulseBoundaryTCPlusOne
+ 
 
 echo.
 
